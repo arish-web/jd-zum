1f53a6201bde7423ebe1ddddf5863845c3c6741b
Marish <marishalagiri@gmail.com>
2025-06-29 16:48:04 +0530

    navbar bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { AuthProvider } from './context/AuthContext';
-import AppRoutes from './routes';
+import { AuthProvider } from "./context/AuthContext";
+import AppRoutes from "./routes";
+import { useStore } from "./store";
+import { useEffect } from "react";
 
 function App() {
+  const setCurrentUser = useStore((state) => state.setCurrentUser);
+  const setToken = useStore((state) => state.setToken);
+
+  useEffect(() => {
+    const storedUser = sessionStorage.getItem("currentUser");
+    const storedToken = sessionStorage.getItem("token");
+
+    if (storedUser) {
+      setCurrentUser(JSON.parse(storedUser));
+    }
+
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
   return (
     <AuthProvider>
       <AppRoutes />
@@ -9,6 +26,4 @@ function App() {
   );
 }
 
-
 export default App;
-
